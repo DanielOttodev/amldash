3baf0e8e9ecaa3a492cc47391be42f62e8bf4ced
otto281 <otto_281@hotmail.com>
2021-08-04 11:43:47 +1000

    First Commit

---
+++ src/layout/Books.vue
@@ -0,0 +1,20 @@
+<template>
+<v-container>
+<BooksForm />
+
+<BooksList/>
+</v-container>
+</template>
+
+<script>
+import BooksForm from '../components/BooksForm.vue'
+import BooksList from '../components/BooksList.vue'
+export default {
+    
+    name:'Books',
+    components: {
+        BooksForm,
+        BooksList
+    }
+}
+</script>
