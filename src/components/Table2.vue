cd139cd669a2ebc4654689307ad2e5e52a37b92d
danielottodev <otto_281@hotmail.com>
2021-08-08 21:59:33 +1000

    Progress on client focus trantable build

--- src/components/Table2.vue
+++ src/components/Table2.vue
@@ -17,12 +17,14 @@
 
       </v-toolbar>
     </template>
-    <template v-slot:expanded-item="{ headers, item }">
+    <template v-slot:expanded-item="{ headers }">
       <td :colspan="headers.length">
-       <p>{{accountName}}</p>
+       <h3> Account: {{accountName}}</h3>
        <v-list>
       <li>
-        {{ item.Detail }}
+        Total: 
+        {{ tranTotal }}
+        <TransTable v-if="transList" :TranTable="transList"> </TransTable>
         </li>
        </v-list>
       </td>
@@ -30,11 +32,14 @@
   </v-data-table>
 </template>
 <script>
+import TransTable from '../components/TransTable.vue'
   export default {
     props:['alertdata'],
+    components:{
+      TransTable
+    },
     data () {
       return {
-        
         expanded: [],
         singleExpand: false,
         alertHeaders: [
@@ -55,9 +60,35 @@
     },
     computed: {
         accountName : function() { 
-        console.log('computed:'+ this.alertdata[0].Detail.split('#',1)[0])// This.alerts is undefined?
+ 
         return this.alertdata[0].Detail.split('#',1)[0]
-        }    
+        } ,
+        tranTotal: function(){ return this.alertdata[0].Detail.split('#',3)[1]},
+        transList: function(){
+          let list = this.alertdata[0].Detail.split("#")[2]
+          list =  list.replace(")","")
+          list = list.trim()
+          let allTrans = list.split("}")
+          allTrans.shift();
+          console.log(allTrans);
+          let allTxn = [];
+            for( var x = 0; x < allTrans.length ; x++){
+          
+            let splitarr =  allTrans[x].split("|")
+            
+         
+              console.log("splitarr:"+splitarr[0])
+              let myobj = {
+                
+                amount: splitarr[0],
+                transDate : splitarr[1],
+                transId : splitarr[2]
+              }
+              allTxn.push(myobj);
+            }
+          console.log(allTxn);
+          return allTxn;          
+        }   
     },
   }
   
---
+++ src/components/TransTable.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-simple-table dense>
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th class="text-left">
+            Name
+          </th>
+          <th class="text-left">
+            Calories
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="item in TranTable"
+          :key="item.Amount"
+        >
+          <td>{{ item.amount }}</td>
+          <td>{{ item.transDate }}</td>
+          <td>{{ item.transId}}</td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
+</template>
+<script>
+  export default {
+    name: "TranTable",
+    props:['allTrans'],
+    data: () => ({
+        allTrans: this.TranTable
+    })
+
+  }
+</script>
